11af3fd2982242ca27e3fb2a097382fe0dfdf1b9
0936311scogg <141672840+0936311scogg@users.noreply.github.com>
2023-08-07 11:46:15 -0700

    Uploaded main.cpp

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\nI like to eat cheese.\n\n" << std::endl;
+    std::cout << "I don't know the difference between \"\\n\" and \"std::endl\".\nSomething about flushing some buffer?" << std::endl;
+    return 0;
+}
